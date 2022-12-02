e737dac9dd0b4554f2acbcbe8c2df2c05d7e5119
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-12-02 10:41:15 -0600

    added rules to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,60 @@ module.exports = () => {
     plugins: [],
 
     module: {
-      rules: [],
+      rules: [
+        new HtmlWebpackPlugin({
+          template: "./index.html",
+          title: "Text",
+        }),
+
+        new InjectManifest({
+          swSrc: "./src-sw.js",
+          swDest: "src-sw.js",
+        }),
+
+        new WebpackPwaManifest({
+          fingerprints: false,
+          inject: true,
+          name: "text editor",
+          short_name: "textedit",
+          description: "write down your text",
+          background_color: "#335ca3",
+          theme_color: "#225ca3",
+          start_url: "/",
+          publicPath: "/",
+          icons: [
+            {
+              src: path.resolve("src/images/logo.png"),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join("assets", "icons"),
+            },
+          ],
+        }),
+      ],
+
+      module: {
+        rules: [
+          {
+            test: /.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /.m?js$/i,
+            exclude: /node_modules/,
+
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env"],
+                plugins: [
+                  "@babel/plugin-proposal-object-rest-spread",
+                  "@babel/transform-runtime",
+                ],
+              },
+            },
+          },
+        ],
+      },
     },
   };
 };
